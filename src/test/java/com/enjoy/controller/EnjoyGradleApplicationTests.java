3c61bb81712bab8a53006aae5e48f2404c877b77
bjli2018 <bjli2018@126.com>
2020-04-12 09:14:47 +0800

    2020-04-12

---
+++ src/test/java/com/enjoy/controller/EnjoyGradleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enjoy.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnjoyGradleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
